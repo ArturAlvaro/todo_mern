5ec575759c1f213448629f6d1893c1f7180d234a
ArturAlvaro <artur.alvaro73@gmail.com>
2022-02-16 10:28:18 -0300

    Testa todas as rotas do back-end

---
+++ back-end/src/tests/integration/deleteTasks.test.js
@@ -0,0 +1,39 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const app = require('../../api/app');
+
+describe('Testa delete de tarefas', function () {
+  describe('Testa delete com sucesso', function () {
+    let response = {};
+    let newResponse = {};
+    let missingTask = {};
+
+    before(async function () {
+      try {
+        response = await chai.request(app).post('/tasks')
+          .send({ task: 'tarefa a ser excluída' });
+
+        const { body: { _id: id } } = response;
+
+        newResponse = await chai.request(app).delete(`/tasks/${id}`);
+        missingTask = await chai.request(app).delete('/tasks/algo');
+      } catch (e) {
+        console.log(e.message);
+      }
+    });
+
+    it('Verifica rota com id errado', function () {
+      expect(missingTask).to.have.status(500);
+      expect(missingTask.body.err.message).to.be.equal('task not found');
+    });
+
+    it('Retorna status 204', function () {
+      expect(newResponse).to.have.status(204);
+    });
+  });
+});
---
+++ back-end/src/tests/integration/updateTasks.test.js
@@ -0,0 +1,35 @@
+/* eslint-disable mocha/no-hooks-for-single-case */
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const app = require('../../api/app');
+
+describe('Testa updateTasks', function () {
+  let response = {};
+  let newResponse;
+  
+  before(async function () {
+    try {
+      response = await chai.request(app).post('/tasks').send({
+        task: 'minha primeira tarefa',
+      });
+
+      const { body: { _id: id } } = response;
+
+      newResponse = await chai.request(app).put(`/tasks/${id}`).send(
+        { task: 'testando update na nova tarefa' },
+      );
+    } catch (e) {
+      console.log(e.message);
+    }
+  });
+
+  it('Atualiza usuário no banco', function () {
+    expect(newResponse).to.have.status(200);
+    expect(newResponse.body).to.have.all.keys('_id', 'task', 'date');
+  });
+});
