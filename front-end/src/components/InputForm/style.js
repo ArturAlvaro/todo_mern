40f6c7016ebabb8f0003d794a83fa308df073c08
ArturAlvaro <artur.alvaro73@gmail.com>
2022-02-17 09:45:38 -0300

    Adiciona estilizaçao (#8)

--- front-end/src/components/InputForm/style.js
+++ front-end/src/components/InputForm/style.js
@@ -2,12 +2,20 @@ import styled from 'styled-components';
 
 export const Input = styled.input`
   align-items: center;
+  border: none;
   border-radius: 25px;
+  box-shadow: 1px 1px 2px white;
   display: flex;
   flex-flow: nowrap column;
-  height: 5vh;
+  height: 7vh;
   text-align: center;
-  width: 50vw;
+  width: 60vw;
+  &:hover {
+    border: 0px solid white;
+    box-shadow: 1px 1px 2px grey;
+    outline-width: 0;
+    transition-duration: 0.2s;
+  }
 `;
 
 export const Label = styled.label`
@@ -23,6 +31,19 @@ export const Section = styled.section`
 `;
 
 export const Button = styled.button`
-  background-color: black;
-  color: white;
+  background-color: #e7e7e7;
+  border: none;
+  border-radius: 15px;
+  box-shadow: 1px 1px 4px 0px white;
+  color: black;
+  height: 5vh;
+  margin: 5px;
+  width: 20vw;
+  &:hover {
+    background-color: #4CAF50;
+    box-shadow: 1px 1px 4px 0px grey;
+    color: white;
+    font-weight: 600;
+    transition-duration: 0.4s;
+  }
 `;
